de338a0e1be1e52ae653edd6d205caa7046de7fb
Philip Okumu <philipo.okumu@gmail.com>
2021-01-05 23:01:29 +0300

    Added stream listener on root class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:brew_team/models/user.dart';
 import 'package:brew_team/screens/wrapper.dart';
+import 'package:brew_team/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 /*This is the root class that holds every element. 
 *It is called when user opens the app*/
@@ -12,8 +15,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,6 +1,8 @@
+import 'package:brew_team/models/user.dart';
 import 'package:brew_team/screens/authenticate/authenticate.dart';
 import 'package:brew_team/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 /* This class listens for authentication status i.e user is logged in or not
 * If logged in redirects to home.dart. If not, to authenticate.dart
@@ -8,6 +10,8 @@ import 'package:flutter/material.dart';
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user);
     //return either home or authenticate widget
     return Authenticate();
   }
