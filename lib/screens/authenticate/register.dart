7f298c3814ed8269e20566f4e89ac006cf034913
Philip Okumu <philipo.okumu@gmail.com>
2021-01-11 23:57:18 +0300

    Added loading animation to login and register pages

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:brew_team/shared/loading.dart';
 import 'package:brew_team/services/auth.dart';
 import 'package:brew_team/shared/constants.dart';
 
@@ -20,95 +21,107 @@ class _RegisterState extends State<Register> {
   // A key to identify our form and it is associated to the global Formstate key
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   // Declare states for the email and password fields for holding the data
+  // Loading animation when data is being fetched from firebase db
+  bool loading = false;
+
   String email = '';
   String password = '';
   String error = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign up to Brew team'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Sign in'),
-            onPressed: () {
-              //Assign the toggleView to the sign in button
-              widget.toggleView();
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
-        // Form for signing in
-        child: Form(
-          // Keeps track the state of our form and help validate it
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              // For email input
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                // Validator property to validate the text field
-                // when we click submit
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              // For password input
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                // Validator property to validate the text field
-                // when we click submit
-                validator: (val) =>
-                    val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                obscureText: true,
-                onChanged: (val) {
-                  //Set the value to be the entered detail
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () async {
-                  // Validates our form based on current form based at the
-                  // point we click the button
-                  if (_formKey.currentState.validate()) {
-                    // we use dynamic since the info we await to be returned
-                    // can be null or a user
-                    dynamic result = await _auth.registerWithEmailAndPassword(
-                        email, password);
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign up to Brew team'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Sign in'),
+                  onPressed: () {
+                    //Assign the toggleView to the sign in button
+                    widget.toggleView();
+                  },
+                )
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+              // Form for signing in
+              child: Form(
+                // Keeps track the state of our form and help validate it
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    // For email input
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      // Validator property to validate the text field
+                      // when we click submit
+                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    // For password input
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Password'),
+                      // Validator property to validate the text field
+                      // when we click submit
+                      validator: (val) => val.length < 6
+                          ? 'Enter a password 6+ chars long'
+                          : null,
+                      obscureText: true,
+                      onChanged: (val) {
+                        //Set the value to be the entered detail
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      color: Colors.pink[400],
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        // Validates our form based on current form based at the
+                        // point we click the button
+                        if (_formKey.currentState.validate()) {
+                          // Loading animation
+                          setState(() => loading = true);
+                          // we use dynamic since the info we await to be returned
+                          // can be null or a user
+                          dynamic result = await _auth
+                              .registerWithEmailAndPassword(email, password);
 
-                    if (result == null) {
-                      setState(() => error = 'Please supply a valid email');
-                      // Else if successfull, the stream that is listening for changes
-                      // in the wrapper will redirect us to the home page
-                    }
-                  }
-                },
+                          if (result == null) {
+                            setState(
+                                () => error = 'Please supply a valid email');
+                            // Else if successfull, the stream that is listening for changes
+                            // in the wrapper will redirect us to the home page
+                            loading = false;
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ],
+                ),
               ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:brew_team/services/auth.dart';
+import 'package:brew_team/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_team/shared/constants.dart';
 
@@ -24,6 +25,8 @@ class _SignInState extends State<SignIn> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   // Declare error variable
   String error = '';
+  // Loading animation when data is being fetched from firebase db
+  bool loading = false;
 
   // Declare states for the email and password fields for holding the data
   String email = '';
@@ -31,91 +34,102 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign in to Brew team'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            onPressed: () {
-              //Assign the toggleView to the sign in button
-              widget.toggleView();
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
-        // Form for signing in
-        child: Form(
-          // Keeps track the state of our form and help validate it
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              // For email input
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                // Validator property to validate the text field
-                // when we click submit
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              // For password input
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                // Validator property to validate the text field
-                // when we click submit
-                validator: (val) =>
-                    val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                obscureText: true,
-                onChanged: (val) {
-                  //Set the value to be the entered detail
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text(
-                  'Sign in',
-                  style: TextStyle(color: Colors.white),
+    // If loading, show loading animation, else show scaffold content
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign in to Brew team'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Register'),
+                  onPressed: () {
+                    //Assign the toggleView to the sign in button
+                    widget.toggleView();
+                  },
+                )
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+              // Form for signing in
+              child: Form(
+                // Keeps track the state of our form and help validate it
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    // For email input
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      // Validator property to validate the text field
+                      // when we click submit
+                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    // For password input
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Password'),
+                      // Validator property to validate the text field
+                      // when we click submit
+                      validator: (val) => val.length < 6
+                          ? 'Enter a password 6+ chars long'
+                          : null,
+                      obscureText: true,
+                      onChanged: (val) {
+                        //Set the value to be the entered detail
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      color: Colors.pink[400],
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        // Validates our form based on current form based at the
+                        // point we click the button
+                        if (_formKey.currentState.validate()) {
+                          // Loading animation
+                          setState(() => loading = true);
+                          // we use dynamic since the info we await to be returned
+                          // can be null or a user
+
+                          dynamic result =
+                              _auth.signInWithEmailAndPassword(email, password);
+
+                          if (result == null) {
+                            setState(() {
+                              error =
+                                  'Could not sign in with those credentials';
+                              loading = false;
+                            });
+                            // Else if successfull, the stream that is listening for changes
+                            // in the wrapper will redirect us to the home page
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ],
                 ),
-                onPressed: () async {
-                  // Validates our form based on current form based at the
-                  // point we click the button
-                  if (_formKey.currentState.validate()) {
-                    // we use dynamic since the info we await to be returned
-                    // can be null or a user
-
-                    dynamic result =
-                        _auth.signInWithEmailAndPassword(email, password);
-
-                    if (result == null) {
-                      setState(() =>
-                          error = 'Could not sign in with those credentials');
-                      // Else if successfull, the stream that is listening for changes
-                      // in the wrapper will redirect us to the home page
-                    }
-                  }
-                },
               ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
